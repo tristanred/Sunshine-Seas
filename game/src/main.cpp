847a3908433c12447ac5f2f463b111047599048c
Tristan Dubé <tristan.dube@gmail.com>
2019-06-18 20:48:59 -0400

    Various project upgrades
    Added a vscode config for Win32.
    
    Adapted the Module code for the new engine version.
    
    Ignored all Build folders, can be used when syncing through OneDrive
    with multiple configs.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     GameEngine eng;
     eng.Initialize();
 
-    eng.CreateModule<NavigationModule>();
+    eng.ActivateModule<NavigationModule>();
 
 
     eng.Play();
